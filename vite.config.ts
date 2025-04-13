635b7ba40465b00f418e619a0d7ba73c4d805d2e
Ethan Zhang <zhangyuzhi1993@gmail.com>
2025-04-14 06:16:17 +0800

    chore: add mock data

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
+import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [UnoCSS(), react()],
+  plugins: [UnoCSS(), react(), mockDevServerPlugin()],
+  server: {
+    proxy: {
+      '^/api': 'http://example.com/',
+    },
+  },
 })
